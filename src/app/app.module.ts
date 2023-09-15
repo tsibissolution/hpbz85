d99cb4378bf18b58973042ef3bb73f0e8f1a1c8a
tsibissolution <71134156+tsibissolution@users.noreply.github.com>
2023-09-15 20:13:57 +0530

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingRoutingModule } from './app-routing-routing.module';
+import { DisplayModule } from './layout/display/display.module';
 
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      
-    ])
+    AppRoutingRoutingModule,
+    DisplayModule
   ],
   declarations: [
     AppComponent,
