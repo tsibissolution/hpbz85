c62bf9668c8da49f15a8f8bd9c829bb0166e16db
tsibissolution <71134156+tsibissolution@users.noreply.github.com>
2023-09-15 19:48:13 +0530

    2 files modified, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,19 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
+
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      
     ])
   ],
   declarations: [
     AppComponent,
-    TopBarComponent,
-    ProductListComponent
+    
   ],
   bootstrap: [
     AppComponent
