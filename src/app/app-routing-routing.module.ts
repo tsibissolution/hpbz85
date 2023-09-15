c7918ccfbf5ec5a1a08b72e4d1aa5700578309ef
tsibissolution <71134156+tsibissolution@users.noreply.github.com>
2023-09-15 20:34:51 +0530

    4 files modified

--- src/app/app-routing-routing.module.ts
+++ src/app/app-routing-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplayComponent } from './layout/display/display.component';
+import { MasterComponent } from './views/master/master.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DisplayComponent,
+    children: [{ path: '', component: MasterComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingRoutingModule { }
+export class AppRoutingRoutingModule {}
