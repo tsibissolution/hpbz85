49782588a8bd3b7ca4c9a8568d40ac3884a68250
tsibissolution <71134156+tsibissolution@users.noreply.github.com>
2023-09-25 19:06:31 +0530

    3 files modified, 14 added

--- appcourier/src/app/app-routing.module.ts
+++ appcourier/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplayComponent } from './layout/display/display.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: DisplayComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- appcourier/src/app/app.module.ts
+++ appcourier/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayComponent } from './layout/display/display.component';
+import { DisplayModule } from './layout/display/display.module';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DisplayModule
   ],
   providers: [],
   bootstrap: [AppComponent]
