17b9c2857362acf05a4424667fe33e84875a6011
tsibissolution <71134156+tsibissolution@users.noreply.github.com>
2024-02-09 21:51:37 +0530

    1 file modified

--- appcourier/src/app/views/master/transportaccount/transportaccount.component.ts
+++ appcourier/src/app/views/master/transportaccount/transportaccount.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-transportaccount',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./transportaccount.component.css']
 })
 export class TransportaccountComponent {
+  transprtForm!:FormGroup;
 
 }
